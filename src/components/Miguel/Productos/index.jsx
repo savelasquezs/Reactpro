57eb6c273d19478ecbac0e8c231a9cff0aa4d7ce
Miguel <sernamiguel441@gmail.com>
2023-05-26 23:18:03 -0500

    feat: :construction: Se empieza a crear los componentes de los productos y a darle estilos
    Aun sigue en construción

---
+++ src/components/Miguel/Productos/index.jsx
@@ -0,0 +1,25 @@
+import { Icon } from '@iconify/react';
+import './index.css'
+
+function Productos({producto, pedidos, precio_venta, ganancia}) {
+    return (
+        <>
+        <div className='nombres-prodcutos'>
+                <th>Productos</th>
+                <th>Pedidos</th>
+                <th>Precio Venta</th>
+                <th>Ganancia</th>
+        </div>
+        <div>
+            <table className="productos">
+                <th><Icon icon={producto} width="40" height="40" /></th>
+                <th>{pedidos}</th>
+                <th>{precio_venta}</th>
+                <th>{ganancia}</th>
+            </table>
+        </div>
+        </>
+    )
+}
+
+export default Productos
--- src/views/Miguel/index.jsx
+++ src/views/Miguel/index.jsx
@@ -1,4 +1,5 @@
 import Estadisticas from "../../components/Miguel/GenericoEstadistica";
+import Productos from "../../components/Miguel/Productos";
 
 function Miguelucho() {
 	return (
@@ -7,6 +8,9 @@ function Miguelucho() {
 			<Estadisticas icono='foundation:dollar' valor= '12456' titulo='Total venta'/>
 			<Estadisticas icono='material-symbols:order-approve' valor= '1039' titulo='Total pedidos'/>
 			<Estadisticas icono='ph:cursor-light' valor= '199' titulo='Compras'/>
+		</div>
+		<div className="">
+			<Productos producto='game-icons:cement-shoes' pedidos='53110' precio_venta='$113.99' ganancia='212214.70'/>
 		</div>
 		</>
 	);
