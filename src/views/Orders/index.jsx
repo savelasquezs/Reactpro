dd2c5732484a683c30676f004907323e519889a7
Santiago Velásquez <santyvano@outlook.com>
2023-05-29 07:50:01 -0500

    feat: componente barrracompraproducto

--- src/views/Orders/index.jsx
+++ src/views/Orders/index.jsx
@@ -6,10 +6,17 @@ function Orders() {
 		hora: '17:05',
 		productos: [{ nombre: 'ropa vieja', cantidad: 3, subtotal: 125800 }],
 	};
+	const order1 = {
+		docId: 'santy',
+		hora: '15:05',
+		productos: [{ nombre: 'ranchero', cantidad: 20, subtotal: 125800 }],
+	};
 	return (
 		<>
 			<h3>Vista de santy</h3>
+
 			<OrderItem order={order} />
+			<OrderItem order={order1} />
 		</>
 	);
 }
